aa8bc65bec0b9b6a37102c792bdc2b864a0e0aa7
Oskar Jonsson <144224908+ojonss@users.noreply.github.com>
2024-05-06 11:58:27 +0200

    add testServo .cpp file
    Testing of Servo 360 and a joystick

--- Agile project repo/src/main.cpp
+++ Agile project repo/src/main.cpp
@@ -1,14 +1,39 @@
-// Include the Servo library 
-#include <ESP32Servo.h> 
 #include <Arduino.h>
-// Declare the Servo pin 
+#include <ESP32Servo.h>
 
-// Create a servo object 
+int inX= A1;        // X axis value from joystick
+int inServo = 7;    // DIGITAL pin to servo
 
-void setup() { 
-   // We need to attach the servo to the used pin number 
-   Serial.begin(9600);
+int centerXvalue {};
+
+int xAxisVal {};
+int servoVal {};
+
+Servo testServo;    // create servo object
+
+void setup ()
+{
+ Serial.begin (9600); 
+ pinMode(inX, INPUT);
+ testServo.attach(inServo);     //attach srvo object with pin
+ centerXvalue = map(analogRead(inX),  0, 4095, 0, 180);
 }
-void loop(){ 
-   Serial.write("apa\n");
+
+void loop ()
+{
+ 
+ xAxisVal = analogRead (inX);   //analog value between 0 - 4095
+
+          // map *något med xAxisVal*, xAxis min - max, turn min - max
+ servoVal = map(xAxisVal, 0, 4095, (centerXvalue - 15), (centerXvalue + 20));
+
+ testServo.write(servoVal);
+
+
+ Serial.print("X = ");
+ Serial.println (xAxisVal, DEC);
+ Serial.print("map = ");
+ Serial.println(servoVal, DEC);
+
+
 }
