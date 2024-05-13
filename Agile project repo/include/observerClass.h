08f3b2d5b9368a814f5fdda1e76824b74c877525
Johanna1066 <152695438+Johanna1066@users.noreply.github.com>
2024-05-13 15:37:00 +0200

    Add basic code for class

---
+++ Agile project repo/include/observerClass.h
@@ -0,0 +1,67 @@
+class Observer
+{
+private:
+public:
+    Observer(/* args */);
+    ~Observer();
+};
+
+Observer::Observer(/* args */)
+{
+}
+
+Observer::~Observer()
+{
+}
+
+//--------AntiCrash------------
+
+/*IDEA: have high priority in RTOS*/
+
+class AntiCrash : public Observer
+{
+private:
+    int distanceToObject{};
+    int minSafetyDistance{};
+    int maxSafetyDistance{};
+
+public:
+    void checkDistance(int newReading);
+};
+
+void checkDistance(int newReading)
+{
+}
+
+//-------Steering----------
+
+class Steering : public Observer
+{
+private:
+    int engineVelocity{};
+    bool engingeDirection{};
+    int servoDirection{};
+
+public:
+    void updateEngineVelocity(int newReading);
+    void updateEngineDirection();
+    void updateServoDirection(int newReading);
+};
+
+void updateEngineVelocity(int newReading)
+{
+
+    // IDEA: Map new reading with posible velocities
+    // IDEA: Update engineVeclocity
+}
+
+void updateEngineDirection()
+{
+}
+
+void updateServoDIrection(int newReading)
+{
+
+    // IDEA: Map new reading with posible directions
+    // IDEA: Update servoDIrection
+}
