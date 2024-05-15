b7f4e8519197694d34d74d94bc4907419721c9a6
Johanna <152695438+Johanna1066@users.noreply.github.com>
2024-05-15 10:12:21 +0200

    Update main.cpp
    remove testing code for engine

--- agileProjectRepo/src/main.cpp
+++ agileProjectRepo/src/main.cpp
@@ -2,20 +2,17 @@
 #include <iostream>
 
 #include "motorClass.h"
-// #include "sensorClass.h"
-// #include "observerClass.h"
+#include "sensorClass.h"
+#include "observerClass.h"
 
-Engine right(A1, D6, D5);
+
 
 void setup()
 {
-  right.intitateEngine();
+
 }
 
 void loop()
 {
-  right.setVelocity(255);
-  delay(5000);
-  right.setVelocity(0);
-  delay(5000);
+
 }
