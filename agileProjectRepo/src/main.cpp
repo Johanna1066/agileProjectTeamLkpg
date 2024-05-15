bdd5e8d94ff5a25c39fd1414a7a2db29704af82d
ojonss <oskar.jonsson@chasacademy.se>
2024-05-15 11:50:35 +0200

    add test code in main.cpp

--- agileProjectRepo/src/main.cpp
+++ agileProjectRepo/src/main.cpp
@@ -6,6 +6,7 @@
 // #include "observerClass.h"
 
 Engine right(A1, D6, D5);
+engineServo carStearing(A2, 90);  //just tmp code
 
 void setup()
 {
@@ -14,8 +15,10 @@ void setup()
 
 void loop()
 {
+  carStearing.setDirection(115);
   right.setVelocity(255);
   delay(5000);
+  carStearing.setDirection(75);
   right.setVelocity(0);
   delay(5000);
 }
