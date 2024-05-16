388e2df8032e55cdb3f2f749f96c079f4d3ecec6
Johanna1066 <152695438+Johanna1066@users.noreply.github.com>
2024-05-16 15:51:08 +0200

    add stopEngine and SetEngineVelocity functions

--- agileProjectRepo/src/main.cpp
+++ agileProjectRepo/src/main.cpp
@@ -1,12 +1,21 @@
 #include <Arduino.h>
 #include <iostream>
-
+#include <vector>
 #include "motorClass.h"
 #include "sensorClass.h"
 // #include "observerClass.h"
 
+
+std::vector<Engine> engines;
+
 Engine right(A1, D6, D5);
 Engine left(A2, D4, D3);
+
+
+
+void stopEngines();
+void setEnginesVelocity(int);
+
 
 USsensor mySensor(D9, D10);
 
@@ -14,6 +23,9 @@ long reading;
 
 void setup()
 {
+  engines.push_back(left);
+  engines.push_back(right);
+
   right.intitateEngine();
   left.intitateEngine();
   mySensor.USsensorInitiate();
@@ -28,16 +40,28 @@ void loop()
 
   if(reading < 20)
   {
-
-    left.setVelocity(0);
-    right.setVelocity(0);
+    stopEngines();
   }
   else
   {
-    left.setVelocity(255);
-    right.setVelocity(255);
+    setEnginesVelocity(255);
   }
 
-
-
 }
+
+void stopEngines()
+{
+  for (auto &engine : engines) 
+  {
+    engine.setVelocity(0);
+  }
+}
+
+void setEnginesVelocity(int velocity)
+{
+  for (auto &engine : engines) 
+  {
+    engine.setVelocity(velocity);
+  }
+}
+
