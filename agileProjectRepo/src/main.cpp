607025afa9ba190df475cae06a182b89603bdb52
Johanna1066 <152695438+Johanna1066@users.noreply.github.com>
2024-05-16 14:44:39 +0200

    add left engine object to test code

--- agileProjectRepo/src/main.cpp
+++ agileProjectRepo/src/main.cpp
@@ -6,16 +6,20 @@
 // #include "observerClass.h"
 
 Engine right(A1, D6, D5);
+Engine left(A2, D4, D3);
 
 void setup()
 {
   right.intitateEngine();
+  left.intitateEngine();
 }
 
 void loop()
 {
   right.setVelocity(255);
+  left.setVelocity(255);
   delay(5000);
   right.setVelocity(0);
+  left.setVelocity(0);
   delay(5000);
 }
