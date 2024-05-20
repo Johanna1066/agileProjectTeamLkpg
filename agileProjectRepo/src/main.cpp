5218b7218b94ceac85a99094fd627c267a4ca45b
Philip <philipjonsson02@gmail.com>
2024-05-20 11:47:17 +0200

    Add testing code for RTOS

--- agileProjectRepo/src/main.cpp
+++ agileProjectRepo/src/main.cpp
@@ -4,11 +4,48 @@
 #include "motorClass.h"
 #include "sensorClass.h"
 
-#include "FreeRTOS.h"
+int count1 = 0;
+int count2 = 0;
 
-// #include "observerClass.h"
+void task1(void * parameters){
+  for(;;){
+    Serial.print("Task 1 counter: ");
+    Serial.println(count1++);
+    vTaskDelay(1000/portTICK_PERIOD_MS);
+  }
+}
 
-void app_main(){
+void task2(void * parameters){
+  for(;;){
+    Serial.print("Task 2 counter: ");
+    Serial.println(count2++);
+    vTaskDelay(1000/portTICK_PERIOD_MS);
+  }
+}
+
+void setup(){
+  Serial.begin(9600);
+
+  xTaskCreate(
+    task1,  //Function name
+    "*Task 1", //Task name
+    1000, //Stack size
+    NULL, //Task parameters
+    1, //Task priority
+    NULL  //Task handle
+  );
+   xTaskCreate(
+    task2,    //Function name
+    "*Task 2", //Task name
+    1000,     //Stack size
+    NULL,     //Task parameters
+    1,        //Task priority
+    NULL      //Task handle
+  );
+}
+
+void loop()
+{
 
 }
 
