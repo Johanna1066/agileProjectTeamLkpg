67b6f10b2c3380ac6064ac9ee37332033a4d4582
Philip <philipjonsson02@gmail.com>
2024-05-22 13:36:23 +0200

    TaskstateTesting

--- agileProjectRepo/src/main.cpp
+++ agileProjectRepo/src/main.cpp
@@ -2,15 +2,17 @@
 #include <iostream>
 #include <vector>
 
+
 #include "Motors/Engine.h"
 #include "Motors/SteeringServo.h"
 #include "Sensors/Joystick.h"
 #include "Sensors/USsensor.h"
 #include "steeringFunctions.h"
 
+
 void stopEngines();
 void setEnginesVelocity(int);
-void initate();
+void initiate(); 
 
 void readSensor(void *parameters)
 {
@@ -25,30 +27,58 @@ void printSensorReading(void *parameters)
 {
   for (;;)
   {
-    reading = mySensor.getDistance();
-    Serial.println(reading);
-    vTaskDelay(500 / portTICK_PERIOD_MS);
+      reading = mySensor.getDistance();
+      Serial.println(reading);
+      vTaskDelay(500 / portTICK_PERIOD_MS);
   }
 }
 
+
+//TaskState Testing
+eTaskState hej{};
+TaskHandle_t readSensorHandle;
+TaskStatus_t xTaskDetails;
+
+void sendRadioSignal(void *parameters) //Testing 
+{
+  for (;;)
+  {
+   
+   
+    // Task Status Test
+    
+
+    readSensorHandle = xTaskGetHandle("*readSensor");
+    configASSERT(readSensorHandle);
+    vTaskGetInfo(readSensorHandle, &xTaskDetails, pdTRUE, eInvalid);
+    //-------------------
+    hej = xTaskDetails.eCurrentState;
+    Serial.println(hej);
+    
+  }
+}
+
+//--------------------------
+
+
 void setup()
 {
+  initiate();
   Serial.begin(9600);
-  mySensor.initiateUSsensor();
 
   xTaskCreate(
       readSensor,    // Function name
       "*readSensor", // Task name
-      2000,          // Stack size
+      1000,          // Stack size
       NULL,          // Task parameters
       1,             // Task priority
-      NULL           // Task handle
+      &readSensorHandle           // Task handle
   );
 
   xTaskCreate(
       printSensorReading,    // Function name
       "*printSensorReading", // Task name
-      2000,                  // Stack size
+      1000,                  // Stack size
       NULL,                  // Task parameters
       1,                     // Task priority
       NULL                   // Task handle
