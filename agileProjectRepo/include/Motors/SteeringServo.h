f59663015ebdacd6a4a21c96c32e633ce1bf6649
ojonss <oskar.jonsson@chasacademy.se>
2024-05-24 10:56:29 +0200

    restructure SteeringServo

--- agileProjectRepo/include/Motors/SteeringServo.h
+++ agileProjectRepo/include/Motors/SteeringServo.h
@@ -1,13 +1,19 @@
-class SteeringServo
+#include <ESP32Servo.h>
+#include "steeringFunctions.h"
 
+
+class SteeringServo
 {
 private:
-    // TODO: Add PINs
     int direction{};
+    int servoPIN{};
+
+    Servo myServo;
 
 public:
-    SteeringServo()
+    SteeringServo(int engineServoPIN)
     {
+        servoPIN = engineServoPIN;
     }
 
     ~SteeringServo()
@@ -15,8 +21,35 @@ public:
         // delete direction;
     }
 
+    void ServoInitiate()
+    {
+        myServo.attach(servoPIN);
+       // myServo.setPeriodHertz(50);
+    }
+
     void setDirection(int newDirection)
     {
         // IDEA: Decide velocity by moving the pointer in tiny steps with delay??
+        myServo.write(newDirection);
+
+
+        /*
+        Maybe something like this to move turning in tiny steps
+
+        int currentPosition = myServo.read();
+
+        if(newDirection > direction && minTurn > direction)
+        {
+            direction--;
+            myServo.write(direction);
+            delay(10;)
+        }
+        if else(newDirection < direction && maxTurn < direction)
+        {
+            direction++;
+            myServo.write(direction);
+            delay(10);
+        }
+        */
     }
 };
