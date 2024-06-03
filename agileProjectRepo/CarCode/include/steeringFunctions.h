138eb8cfbbc3a80e7346bd4361dc453df06395bb
Johanna1066 <152695438+Johanna1066@users.noreply.github.com>
2024-06-03 09:48:41 +0200

    add correct code to steeringFunctions

--- agileProjectRepo/CarCode/include/steeringFunctions.h
+++ agileProjectRepo/CarCode/include/steeringFunctions.h
@@ -2,33 +2,34 @@
 
 std::vector<Engine> engines;
 
-Engine right(A1, D6, D5);
-Engine left(A2, D4, D3);
+Engine right(A1, D5, D6);
+Engine left(A2, D3, D4);
 
-USsensor mySensor(D9, D10);
-long reading;
+SteeringServo myServo(9);
+
+USsensor mySensor(D8, D7); // TODO: Change to correct PINs
+
+int reading{};
+int dataRecieved{};
+
+int engineSpeed{};
+uint8_t direction{};
 bool hinderForwardMovement = false;
 
 void initiate()
 {
+    myServo.initiateServo();
     engines.push_back(left);
     engines.push_back(right);
     right.intitateEngine();
     left.intitateEngine();
     mySensor.initiateUSsensor();
 }
-void stopEngines()
-{
-    for (auto &engine : engines)
-    {
-        engine.setVelocity(0);
-    }
-}
 
-void setEnginesVelocity(int velocity)
+void setEnginesVelocity(int velocity, bool obsticle)
 {
     for (auto &engine : engines)
     {
-        engine.setVelocity(velocity);
+        engine.setVelocity(velocity, obsticle);
     }
 }
